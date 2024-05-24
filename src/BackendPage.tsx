ae3d82f04f8644df3fb6408882be2dbf627c1767
Awe Ayomidipupo <pupoawe@gmail.com>
2024-05-24 19:20:53 +0100

    fix stuff

--- src/BackendPage.tsx
+++ src/BackendPage.tsx
@@ -1,25 +1,19 @@
-import { Input } from "@nextui-org/react"
-import { createBrowserNavigatorFromLocation } from "@tma.js/sdk"
-
-const navigator = createBrowserNavigatorFromLocation({
-	hashMode: "slash"
-});
-navigator.attach();
+import { Input } from "@nextui-org/react";
 
 export default function BackendPage() {
-	return (
-		<>
-			<Input
-				type="url"
-				label="Website"
-				placeholder="nextui.org"
-				labelPlacement="outside"
-				startContent={
-					<div className="pointer-events-none flex items-center">
-						<span className="text-default-400 text-small">https://</span>
-					</div>
-				}
-			/>
-		</>
-	)
+  return (
+    <>
+      <Input
+        type="url"
+        label="Website"
+        placeholder="nextui.org"
+        labelPlacement="outside"
+        startContent={
+          <div className="pointer-events-none flex items-center">
+            <span className="text-default-400 text-small">https://</span>
+          </div>
+        }
+      />
+    </>
+  );
 }
